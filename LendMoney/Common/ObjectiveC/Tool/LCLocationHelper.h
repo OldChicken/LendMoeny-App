c8dc411cfed9f0a9324a1fa7e8d59492e03a28de
Lechech <505349486@qq.com>
2019-04-15 17:11:23 +0800

    地理围栏优化
    类型：优化
    描述：地理围栏优化
    提交人：lcc
    日期：190415

--- LendMoney/Common/ObjectiveC/Tool/LCLocationHelper.h
+++ LendMoney/Common/ObjectiveC/Tool/LCLocationHelper.h
@@ -17,6 +17,7 @@
 + (instancetype)sharedInstance;
 
 
+#pragma mark - Geofence Authority
 /**
  是否始终允许定位
  */
@@ -32,6 +33,8 @@
 - (void)requestAlwaysLocationPermissions:(BOOL)always completion:(void (^)(BOOL granted))completion;
 
 
+
+#pragma mark - Geofence Local Api
 /**
  添加地理围栏
  
@@ -56,7 +59,7 @@
 - (void)removeGeofence:(NSString *)identifier;
 
 
-
+#pragma mark - Geofence Push
 /**
  发送地理围栏推送
  
@@ -71,4 +74,69 @@
  @enterFence    是否进入围栏
  */
 - (void)registerAPN;
+
+
+
+#pragma mark - Geofence Web Server
+/**
+ 从平台拉取地理围栏信息
+ 
+ @param presetId
+ @param needAotuStart   获取成功后，是否需要自动开启监测
+ @param completion isOpen为YES时表示围栏开启
+ */
+- (void)getGeofenceInfoFormServer:(NSString *)presetId
+                    needAotuStart:(BOOL)needAotuStart
+                     needAlertTip:(BOOL)needAletTip
+                       completion:(void (^)(BOOL isOpen))completion;
+
+
+/**
+ 保存地理围栏信息到平台
+ 
+ @param presetId 围栏ID
+ @param longitude 经度
+ @param presetId 维度
+ @param range 直径
+ @param isOpen 使能
+ @param completion success为YES时表示成功
+ */
+- (void)setGeoFencePreset:(NSString *)presetId
+                longitude:(NSString *)longitude
+                 latitude:(NSString*)latitude
+                    range:(NSString*)range
+                   isOpen:(BOOL)isOpen
+               completion:(void (^)(BOOL))completion;
+
+
+/**
+ 保存行为配置表到平台
+ 
+ @param presetId 围栏ID
+ @param completion success为YES时表示成功
+ */
+- (void)setGeofenceActionToServer:(NSString *)presetId
+                       actionType:(NSString *)actionType
+                        devAction:(NSArray *)devAction
+                       completion:(void (^)(BOOL))completion;
+
+
+/**
+ 地理围栏触发上报
+ 
+ @param presetId 围栏ID
+ @isinside       围栏内外，YES/NO:围栏内/围栏外
+ @param completion success为YES时表示成功
+ */
+- (void)geofenceImplent:(NSString *)presetId isinside:(BOOL)inside completion:(void (^)(BOOL))completion;
+
+
+/**
+ 清除某个设备配置
+ 
+ @param presetId 围栏ID
+ @param deviceId 设备ID
+ @param completion success为YES时表示成功
+ */
+- (void)deleteDevice:(NSString *)deviceId;
 @end
--- LendMoney/Common/ObjectiveC/Tool/LCLocationHelper.m
+++ LendMoney/Common/ObjectiveC/Tool/LCLocationHelper.m
@@ -47,6 +47,7 @@
     return self;
 }
 
+#pragma mark - Geofence Authority
 - (BOOL)isAlwaysAllowLocation {
     
     return CLLocationManager.authorizationStatus == kCLAuthorizationStatusAuthorizedAlways ? YES : NO;
@@ -99,6 +100,7 @@
 }
 
 
+#pragma mark - Geofence Local Api
 - (void)addGeofence:(double)longitude
            latitude:(double)latitude
              radius:(long)radius
@@ -162,7 +164,8 @@
     
 }
 
-#pragma mark - Local Push
+
+#pragma mark - Geofence Push
 // 注册通知
 - (void)registerAPN {
 
@@ -211,10 +214,45 @@
     }
 }
 
+#pragma mark - Geofence Alert
+- (void)insideGeofenceAlert {
+    
+    if ([[UIApplication sharedApplication] applicationState] == UIApplicationStateActive) {
+        NSLog(@"进入监测区域!");
+        UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"Geofence Warning"
+                                                                         message:@"Enter the Geofence"
+                                                                  preferredStyle:UIAlertControllerStyleAlert];
+        UIViewController *currentVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+        
+        UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [alertVC addAction:confirmAction];
+        
+        [currentVC presentViewController:alertVC animated:YES completion:nil];
+    }
+}
+
+
+- (void)outsideGeofenceAlert {
+    
+  if ([[UIApplication sharedApplication] applicationState] == UIApplicationStateActive) {
+      NSLog(@"离开监测区域!");
+      UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"Geofence Warning"
+                                                                       message:@"Outside the Geofence"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+      
+      UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+      [alertVC addAction:confirmAction];
+      
+      UIViewController *currentVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+      [currentVC presentViewController:alertVC animated:YES completion:nil];
+  }
+    
+}
+
+
 #pragma mark - CLLocationManagerDelegate
 - (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region {
-     NSLog(@"开始监听");
-    
+    NSLog(@"开始监听");
 }
 
 // 进入指定区域
@@ -222,18 +260,10 @@
         didEnterRegion:(CLRegion *)region {
     
     NSLog(@"进入监测区域!");
+    //执行设备行为
+   
     if ([[UIApplication sharedApplication] applicationState] == UIApplicationStateActive) {
-        NSLog(@"进入监测区域!");
-        UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"地理围栏监测"
-                                                                         message:@"您已进入监测区域"
-                                                                  preferredStyle:UIAlertControllerStyleAlert];
-        UIViewController *currentVC = [UIApplication sharedApplication].keyWindow.rootViewController;
-        
-        UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
-        [alertVC addAction:confirmAction];
-        
-        [currentVC presentViewController:alertVC animated:YES completion:nil];
-
+        [self insideGeofenceAlert];
     }else{
         [self sendLocalNoticeEnterGeofence:YES];
     }
@@ -243,22 +273,12 @@
 // 离开指定区域
 -(void)locationManager:(CLLocationManager *)manager
          didExitRegion:(CLRegion *)region {
-   
+    
     NSLog(@"离开监测区域!");
     if ([[UIApplication sharedApplication] applicationState] == UIApplicationStateActive) {
-        NSLog(@"离开监测区域!");
-        UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"地理围栏监测"
-                                                                         message:@"您已离开监测区域"
-                                                                  preferredStyle:UIAlertControllerStyleAlert];
-        
-        UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
-        [alertVC addAction:confirmAction];
-        
-        UIViewController *currentVC = [UIApplication sharedApplication].keyWindow.rootViewController;
-        [currentVC presentViewController:alertVC animated:YES completion:nil];
-
+        [self outsideGeofenceAlert];
     }else{
-       [self sendLocalNoticeEnterGeofence:NO];
+        [self sendLocalNoticeEnterGeofence:NO];
     }
 }
 
@@ -267,8 +287,9 @@
     NSLog(@"监听失败,error = %@",error.description);
 }
 
+//定位回调(每次添加围栏后，定位成功时会立即进入此方法，可以用于设置围栏成功后立即下发配置)
 - (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region{
-
+    
     switch (state) {
         case CLRegionStateUnknown:
             break;
@@ -276,40 +297,221 @@
         {
             //当前在围栏内部
             NSLog(@"当前在在监测区域内!");
-            UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"地理围栏监测"
-                                                                             message:@"您已处于监测区域内"
-                                                                      preferredStyle:UIAlertControllerStyleAlert];
-            
-            UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
-            [alertVC addAction:confirmAction];
-            
-            UIViewController *currentVC = [UIApplication sharedApplication].keyWindow.rootViewController;
-            [currentVC presentViewController:alertVC animated:YES completion:nil];
+            [self insideGeofenceAlert];
         }
-         
+            
             break;
         case CLRegionStateOutside:
         {
             //当前在围栏外部
             NSLog(@"当前在监测区域外!");
-            UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"地理围栏监测"
-                                                                             message:@"您已离开监测区域"
-                                                                      preferredStyle:UIAlertControllerStyleAlert];
-            
-            UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
-            [alertVC addAction:confirmAction];
-            
-            UIViewController *currentVC = [UIApplication sharedApplication].keyWindow.rootViewController;
-            [currentVC presentViewController:alertVC animated:YES completion:nil];
+            [self outsideGeofenceAlert];
         }
             break;
-
+            
         default:
             break;
     }
     
+}
+
+#pragma mark - Geofence Web Server
+- (void)getGeofenceInfoFormServer:(NSString *)presetId
+                    needAotuStart:(BOOL)needAotuStart
+                     needAlertTip:(BOOL)needAletTip
+                       completion:(void (^)(BOOL isOpen))completion {
+    
+    
+//    [LCUserInterface saas_getPresetDeviceActionConfig:presetId success:^(NSArray<LCGeofenceInfo *> * result) {
+//
+//        if (result.count > 0) {
+//            //目前只有一个预置点
+//            self.geofenceInfo = result.firstObject;
+//            NSMutableArray *homeActions = [[NSMutableArray alloc]initWithCapacity:2];
+//            NSMutableArray *awayActions = [[NSMutableArray alloc]initWithCapacity:2];
+//            for (int i = 0; i < result.count; i++) {
+//                //这个地方平台设计的有缺陷，需要优化，应该将home和away的行为表放在一起返回，不应该分开
+//                LCGeofenceInfo *info = result[i];
+//                for (int j = 0; j < info.devAction.count; j++) {
+//                    if ([info.actionType  isEqual: GEOFENCE_ACTION_TYPE_HOME]) {
+//                        [homeActions addObject:info.devAction[j]];
+//                    }else if ([info.actionType  isEqual: GEOFENCE_ACTION_TYPE_AWAY]) {
+//                        [awayActions addObject:info.devAction[j]];
+//                    }
+//                }
+//                self.homeActions = homeActions;
+//                self.awayActions = awayActions;
+//            }
+//        }else {
+//            [self removeGeofence];
+//            self.geofenceInfo = nil;
+//            self.homeActions = nil;
+//            self.awayActions = nil;
+//        }
+//
+//        if ([self isAlwaysAllowLocation] && self.geofenceInfo.isOpen && needAotuStart) {
+//            //如果已经开启定位，且已经设置过围栏，且需要自动开启监测
+//            [self addGeofence:self.geofenceInfo.longitude.doubleValue
+//                     latitude:self.geofenceInfo.latitude.doubleValue
+//                       radius:self.geofenceInfo.range.doubleValue/2.0
+//                   identifier:self.geofenceInfo.presetId completion:^(BOOL granted) {
+//                       NSLog(@"开启地理围栏成功");
+//                   }];
+//        }
+//
+//        if (!self.geofenceInfo.isOpen) {
+//            [self removeGeofence];
+//        }
+//
+//        if (completion) {
+//            completion(YES);
+//        }
+//    } failure:^(LCError *error) {
+//        NSLog(@"%@", error);
+//        if (completion) {
+//            completion(NO);
+//        }
+//    }];
     
 }
+
+
+- (void)setGeoFencePreset:(NSString *)presetId
+                longitude:(NSString *)longitude
+                 latitude:(NSString*)latitude
+                    range:(NSString*)range
+                   isOpen:(BOOL)isOpen
+               completion:(void (^)(BOOL))completion {
+    
+    
+//    if(range.doubleValue  > GEOFENCE_MAXIMUM_RADIUS * 2) {
+//        range = [NSString stringWithFormat:@"%d",GEOFENCE_MAXIMUM_RADIUS * 2];
+//        NSLog(@"radius is over limit!");
+//    }
+//
+//    if(range.doubleValue < GEOFENCE_MINIMUM_RADIUS * 2) {
+//        range = [NSString stringWithFormat:@"%d",GEOFENCE_MINIMUM_RADIUS * 2];
+//        NSLog(@"radius is over limit!");
+//    }
+//
+//    [LCUserInterface saas_setGeoFencePreset:presetId longitude:longitude latitude:latitude range:range isOpen:isOpen success:^{
+//
+//        //开启弹框提示
+//        self.needAlertTip = YES;
+//
+//        if (isOpen) {
+//            //保存成功
+//            [self addGeofence:longitude.doubleValue
+//                     latitude:latitude.doubleValue
+//                       radius:range.doubleValue/2.0
+//                   identifier:presetId completion:^(BOOL granted) {
+//                       NSLog(@"开启地理围栏成功,更新本地信息");
+//                       LCGeofenceInfo *updateInfo =  [LCGeofenceInfo new];
+//                       updateInfo.longitude = longitude;
+//                       updateInfo.latitude = latitude;
+//                       updateInfo.isOpen = isOpen;
+//                       updateInfo.range = range;
+//                       updateInfo.presetId = presetId;
+//                       self.geofenceInfo = updateInfo;
+//                   }];
+//        }else {
+//            [self removeGeofence];
+//            self.geofenceInfo = nil;
+//        }
+//        if (completion) {
+//            completion(YES);
+//        }
+//
+//    } failure:^(LCError *error) {
+//
+//        NSLog(@"%@", error);
+//        if (completion) {
+//            completion(NO);
+//        }
+//
+//    }];
+    
+}
+
+- (void)setGeofenceActionToServer:(NSString *)presetId
+                       actionType:(NSString *)actionType
+                        devAction:(NSArray *)devAction
+                       completion:(void (^)(BOOL))completion {
+    
+//    [LCUserInterface saas_setPresetDeviceActionConfig:presetId actionType:actionType devAction:devAction success:^{
+//
+//        if ([actionType  isEqual: GEOFENCE_ACTION_TYPE_AWAY]) {
+//            self.awayActions = devAction;
+//        }
+//
+//        if ([actionType  isEqual: GEOFENCE_ACTION_TYPE_HOME]) {
+//            self.homeActions = devAction;
+//        }
+//        if (completion) {
+//            completion(YES);
+//        }
+//
+//
+//    } failure:^(LCError *error) {
+//
+//        NSLog(@"%@", error);
+//        if (completion) {
+//            completion(NO);
+//        }
+//
+//    }];
+//
+}
+
+- (void)geofenceImplent:(NSString *)presetId isinside:(BOOL)inside completion:(void (^)(BOOL))completion {
+    
+//    NSString *type = inside == YES ? GEOFENCE_ACTION_TYPE_HOME : GEOFENCE_ACTION_TYPE_AWAY;
+//    [LCUserInterface saas_implementPresetAction:presetId actionType:type success:^{
+//        if (completion) {
+//            completion(YES);
+//        }
+//    } failure:^(LCError *error) {
+//        NSLog(@"%@", error);
+//        if (completion) {
+//            completion(NO);
+//        }
+//    }];
+    
+}
+
+- (void)deleteDevice:(NSString *)deviceId {
+    
+    
+//    NSArray *fitHomeArray = [self.homeActions monad_Filter:^BOOL(id _Nonnull obj) {
+//
+//        if ([obj isKindOfClass:LCGeofenceDeviceActionInfo.class]) {
+//            LCGeofenceDeviceActionInfo *info = (LCGeofenceDeviceActionInfo*)obj;
+//            if ([info.deviceId isEqualToString:deviceId] ) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }];
+//
+//    NSArray *fitAwayArray = [self.awayActions monad_Filter:^BOOL(id _Nonnull obj) {
+//
+//        if ([obj isKindOfClass:LCGeofenceDeviceActionInfo.class]) {
+//            LCGeofenceDeviceActionInfo *info = (LCGeofenceDeviceActionInfo*)obj;
+//            if ([info.deviceId isEqualToString:deviceId] ) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }];
+//
+//
+//    [self setGeofenceActionToServer:self.geofenceInfo.presetId actionType:GEOFENCE_ACTION_TYPE_AWAY devAction:fitAwayArray completion:nil];
+//
+//    [self setGeofenceActionToServer:self.geofenceInfo.presetId actionType:GEOFENCE_ACTION_TYPE_HOME devAction:fitHomeArray completion:nil];
+    
+    
+}
+
 
 
 @end
